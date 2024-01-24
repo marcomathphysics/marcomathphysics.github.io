795ad8e69b6091d10f21c378e5001ae7c3a05975
marcoMathPhysics <139377971+marcoMathPhysics@users.noreply.github.com>
2024-01-23 21:33:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var List=["","Te","amo","mucho","muucho"]
 let  count=0;
 function changeMe() {
-	var div = document.getElementById('divd').innerHTML;
-	count++;
-	document.getElementById('divd').innerHTML = List[count];
+    var div = document.getElementById('divd').innerHTML;
+    count++;
+    if (count === 5) {
+       count=1;
+        document.getElementById('divd').innerHTML = List[count];
+    } else {
+          document.getElementById('divd').innerHTML = List[count];
+    }
+ 
+    
 }
