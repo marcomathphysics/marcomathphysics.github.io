6dba7a4479e90106d80c48708da3a13ef67d94d6
marcoMathPhysics <139377971+marcoMathPhysics@users.noreply.github.com>
2024-01-23 21:39:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let  count=0;
 function changeMe() {
     var div = document.getElementById('divd').innerHTML;
     count++;
-    if (count === 5) {
+    if (count === 4) {
        count=1;
         document.getElementById('divd').innerHTML = List[count];
     } else {
