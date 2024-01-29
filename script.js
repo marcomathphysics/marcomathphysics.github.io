6ccc6aac2588ead9c5ec5f6653cafed4a08eb0b9
Marco <139377971+marcomathphysics@users.noreply.github.com>
2024-01-29 19:59:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var List=["","Te","amo","mucho","muucho", "Nubecita" ]
+let  count=0;
+function changeMe() {
+    var div = document.getElementById('divd').innerHTML;
+    count++;
+    if (count === 6) {
+       count=1;
+        document.getElementById('divd').innerHTML = List[count];
+    } else {
+          document.getElementById('divd').innerHTML = List[count];
+    }
+ 
+    
+}
